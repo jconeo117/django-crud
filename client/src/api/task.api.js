1cc8b49c7b5f296d3a1f7ef157d6a2da2797a311
Jhon coneo <jconeo117@gmail.com>
2023-09-08 06:26:16 -0300

    update url api

--- client/src/api/task.api.js
+++ client/src/api/task.api.js
@@ -1,8 +1,14 @@
 import axios from 'axios'
 
+// eslint-disable-next-line no-undef
+const baseURL = process.env.NODE_ENV === 'production'
+  ? 'https://vacancy-management.onrender.com/vacancy/api/v1/vacancys/'
+  : 'http://localhost:8000/vacancy/api/v1/vacancys/';
+
 const vacancyApi = axios.create({
-  baseURL: 'http://localhost:8000/vacancy/api/v1/vacancys/'
+  baseURL: baseURL
 })
+
 export const GetAllVacancys = () =>vacancyApi.get('/')
 export const GetVacancy = (id) =>vacancyApi.get('/'+id)
 export const CreateVacancys = (vacancy) =>vacancyApi.post('/', vacancy)
